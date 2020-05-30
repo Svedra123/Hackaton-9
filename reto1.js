2884145a2eee5a76dc207d785b7469e47a3e294a
Diego Saavedra Villaran <diegosaavedra123@hotmail.com>
2020-05-30 10:02:04 -0500

    reto1

--- reto1.js
+++ reto1.js
@@ -8,16 +8,15 @@ console.log(cipherText)
 
 for (let i = 0; i <= cipherText.length; i++) {
 
-    if (cipherText[i]=35) {
-        a = i
-    }
-
-
     a = cipherText.charCodeAt(i) + 3
     b = String.fromCharCode(a)
     
+    if (cipherText.charCodeAt[i]=32) {
+        a = cipherText.charCodeAt(i)
+    }
 
     console.log (b)
+
 
 }
 
